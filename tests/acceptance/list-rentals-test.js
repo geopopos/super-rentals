a1f1f9b22b6424612008fd8f78645f41f824a6ec
Geopopos <groros2@icloud.com>
2019-06-24 18:20:57 -0400

    updated mirage to return properly formatted json data\n updated tests for list-rentals and routes for rental.js to use rental model

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,10 +1,12 @@
 import { module, test } from 'qunit';
 import { click, currentURL, visit } from '@ember/test-helpers';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
-
+  setupMirage(hooks);
+  
   test('should show rentals as the home page.', async function (assert){
     await visit('/');
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
